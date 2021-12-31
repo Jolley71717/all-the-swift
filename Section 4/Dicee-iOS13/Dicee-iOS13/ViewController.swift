f4db42b9525701db2e75d9ba4ae85bf43e0fcaa1
Luke Dutton <duttonluke03@gmail.com>
2021-12-30 17:12:01 -0700

    added the ability to roll the dice and have random numbers generated

--- Section 4/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Section 4/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -16,14 +16,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageView1.alpha =  0.5
-
-        
     }
-
-
+    
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        var diceOps = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        diceImageView1.image = diceOps[Int.random(in: 0...5)]
+        diceImageView2.image = diceOps[Int.random(in: 0...5)]
+    }
 }
